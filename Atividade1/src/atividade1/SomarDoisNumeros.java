c04e7bfcccd68ebb9822013007305156428ea79f
Cadu Melo <cadumelosabino@gmail.com>
2025-02-20 16:08:00 -0300

    Commit atividade1

---
+++ Atividade1/src/atividade1/SomarDoisNumeros.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package atividade1;
+
+/**
+ * 
+ */
+
+import java.util.*;
+
+class SomarDoisNumeros {
+	public static Scanner sc = new Scanner(System.in);
+
+	public static void main (String args[]) {
+		// Delcaracao de variaveis
+		int num1, num2, soma;
+		
+		// Leituras
+		System.out.println("Digite um número");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro número");
+		num2 = sc.nextInt();
+		
+		// Somar
+		soma = num1 + num2;
+		
+		
+		// Mostrar na tela
+		System.out.println("Soma:" + soma);
+		
+	}
+}
+
